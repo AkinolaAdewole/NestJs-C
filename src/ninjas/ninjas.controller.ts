619d0099b2ea4fd790504638e973fa5340ec4bc6
Akinola Adewole <adewoleakinola24@gmail.com>
2024-02-15 11:11:03 +0100

    NextJs

--- src/ninjas/ninjas.controller.ts
+++ src/ninjas/ninjas.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, Post } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 
 @Controller('ninjas')
 export class NinjasController {
@@ -11,12 +11,24 @@ export class NinjasController {
     // Get one by id
     @Get(':id')
     getOneNinja(@Param('id') id: string) {
-        return {};
+        return {message:' Get one successfully'};
     };
 
     //POST
     @Post()
     createNinja(){
         return { message:' post successfully '}
+    };
+
+    //PUT  /ninjas/:id
+    @Put(':id')
+    updateNinja(@Param('id') id:any){
+        return { message: 'update successfully'}
+    };
+
+    //Delete /ninjas/:id
+    @Delete(':id')
+    removeNinja(@Param('id') id:any){
+        return { message: 'delete successfully '}
     }
 }
